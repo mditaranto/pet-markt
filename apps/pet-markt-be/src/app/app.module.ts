2340840382bd5353d76799675aad923d0c9a2bd1
mditaranto <matiasnahuel111@gmail.com>
2025-05-19 10:23:06 +0200

    Configurando graphQL

--- apps/pet-markt-be/src/app/app.module.ts
+++ apps/pet-markt-be/src/app/app.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ProductsModule } from './products/products.module';
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+    }),
+    ProductsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
