aab0b546f5c60aaf30cb6a012bbbcb908f5d2b8c
mditaranto <matiasnahuel111@gmail.com>
2025-05-21 13:10:33 +0200

    Search implementeado con fotos

--- apps/pet-markt-web/src/app/products/products.component.ts
+++ apps/pet-markt-web/src/app/products/products.component.ts
@@ -1,20 +1,37 @@
 import { afterNextRender, Component, inject } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { ProductStore } from '../stores/product.store';
 import { ProductCardComponent } from '../components/product-card/product-card.component';
+import { FormsModule } from '@angular/forms'
+import {  debounceTime, distinctUntilChanged, Subject } from 'rxjs';
+import untilDestroyed from '../utils/untilDestroyed';
 
 @Component({
   selector: 'app-products',
-  imports: [CommonModule, ProductCardComponent],
+  imports: [ProductCardComponent, FormsModule],
   templateUrl: './products.component.html',
   styleUrl: './products.component.scss',
 })
 export class ProductsComponent {
+  searchTerm = '';
   productStore = inject(ProductStore)
+  searchSubject = new Subject<string>();
+  destroyed = untilDestroyed();
 
   constructor() {
     afterNextRender(() => {
-      this.productStore.loadProducts();
+      this.productStore.loadProducts() ;
+      this.searchSubject.pipe(  
+        debounceTime(500),
+        distinctUntilChanged(),
+        this.destroyed()
+      ).subscribe((term) => {
+        console.log({term})
+        this.productStore.searchProducts(term)
+      })
     });
+  }
+
+  onSearch(term: string) {
+    this.searchSubject.next(term)
   }
 }
--- apps/pet-markt-web/src/app/stores/product.store.ts
+++ apps/pet-markt-web/src/app/stores/product.store.ts
@@ -2,11 +2,24 @@ import { inject } from '@angular/core';
 import { patchState, signalStore, withMethods, withState } from '@ngrx/signals';
 import { Product } from '@prisma/client';
 import { Apollo, gql } from 'apollo-angular';
-import { tap } from 'rxjs';
+import { catchError, EMPTY, map, tap } from 'rxjs';
 
 const GET_PRODUCTS = gql`
   query GetProducts {
     products {
+      id
+      name
+      description
+      price
+      image
+      stripePriceId 
+    }
+  }
+`;
+
+const SEARCH_PRODUCTS = gql`
+  query SearchProducts($searchTerm: String!) {
+    searchProducts(term: $searchTerm) {
       id
       name
       description
@@ -38,7 +51,7 @@ export const ProductStore = signalStore(
   withState(initialState),
   withMethods((store, apollo = inject(Apollo)) => ({
     loadProducts() {
-      patchState(store, { loading: true });
+      patchState(store, { loading: true, error: null });
       apollo
         .watchQuery<{ products: Product[] }>({
           query: GET_PRODUCTS,
@@ -50,6 +63,27 @@ export const ProductStore = signalStore(
             error: (error) =>
               patchState(store, { error: error.message, loading: false }),
           })
+        )
+        .subscribe();
+    },
+
+    searchProducts(term: string) {
+      patchState(store, { loading: true, error: null });
+      apollo
+        .query<{ searchProducts: Product[] }>({
+          query: SEARCH_PRODUCTS,
+          variables: {
+            searchTerm: term
+          }
+        })
+        .pipe(
+          map(({ data }) =>
+              patchState(store, { products: data.searchProducts, loading: false })
+        ),
+        catchError((error) => {
+          patchState(store, { error: error.message, loading: false })
+          return EMPTY
+        })
         )
         .subscribe();
     },
